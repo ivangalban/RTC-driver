1cf95c180a5f3d2fc8deb3f41cab943057f5e99a
Ivan Galban Smith <i.galban@lab.matcom.uh.cu>
2016-12-12 22:59:51 -0500

    implemented rtc_get_*() function

--- src/kernel/rtc.c
+++ src/kernel/rtc.c
@@ -1,5 +1,5 @@
 #include <rtc.h>
-
+#include <io.h>
 
 void rtc_init(){
 
@@ -7,30 +7,36 @@ void rtc_init(){
 
 
 u8 rtc_get_seconds(){
-
+	outb(CMOS_ADDRESS, REG_SECONDS);
+	return inb(CMOS_DATA);
 }
 
 
 u8 rtc_get_minutes(){
-
+	outb(CMOS_ADDRESS, REG_MINUTES);
+	return inb(CMOS_DATA);
 }
 
 
 u8 rtc_get_hours(){
-
+	outb(CMOS_ADDRESS, REG_HOURS);
+	return inb(CMOS_DATA);
 }
 
 
 u8 rtc_get_day(){
-
+	outb(CMOS_ADDRESS, REG_DAY);
+	return inb(CMOS_DATA);
 }
 
 
 u8 rtc_get_month(){
-
+	outb(CMOS_ADDRESS, REG_MONTH);
+	return inb(CMOS_DATA);
 }
 
 
 u32 rtc_get_year(){
-
+	outd(CMOS_ADDRESS, REG_YEAR);
+	return ind(CMOS_DATA);
 }
