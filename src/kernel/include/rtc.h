1bab1d2823d7c534ef30416002bfb5daac9cbb58
Ivan Galban Smith <i.galban@lab.matcom.uh.cu>
2016-12-13 13:46:53 -0500

    erase function rtc_get_*()

--- src/kernel/include/rtc.h
+++ src/kernel/include/rtc.h
@@ -18,9 +18,3 @@
 void NMI_enable();
 void NMI_disable();
 void rtc_init();
-u8 rtc_get_seconds();
-u8 rtc_get_minutes();
-u8 rtc_get_hours();
-u8 rtc_get_day();
-u8 rtc_get_month();
-u32 rtc_get_year();
--- src/kernel/time.c
+++ src/kernel/time.c
@@ -3,12 +3,7 @@
 
 //Obtiene la fecha y la hora actuales.
 void time_get(struct tm *t) {
-	t-> seconds = rtc_get_seconds();
-	t-> minutes = rtc_get_minutes();
-	t-> hours 	= rtc_get_hours();
-	t-> day 	= rtc_get_day();
-	t-> month 	= rtc_get_month();
-	t-> year 	= rtc_get_year();
+	
 }
 
 
