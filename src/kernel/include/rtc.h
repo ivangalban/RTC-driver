f4b8e73ce3826479f1f7c968b4b1c07147b8cff7
Ivan Galban Smith <i.galban@lab.matcom.uh.cu>
2016-12-12 22:45:10 -0500

    include prototype of function in rtc

---
+++ src/kernel/include/rtc.h
@@ -0,0 +1,10 @@
+#include <typedef.h>
+
+
+void rtc_init();
+u8 rtc_get_seconds();
+u8 rtc_get_minutes();
+u8 rtc_get_hours();
+u8 rtc_get_day();
+u8 rtc_get_month();
+u32 rtc_get_year();
--- src/kernel/rtc.c
+++ src/kernel/rtc.c
@@ -1,2 +1,36 @@
 #include <rtc.h>
 
+
+void rtc_init(){
+
+}
+
+
+u8 rtc_get_seconds(){
+
+}
+
+
+u8 rtc_get_minutes(){
+
+}
+
+
+u8 rtc_get_hours(){
+
+}
+
+
+u8 rtc_get_day(){
+
+}
+
+
+u8 rtc_get_month(){
+
+}
+
+
+u32 rtc_get_year(){
+
+}
