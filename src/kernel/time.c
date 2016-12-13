ad0a321347c773412336283511186e2d754cdd1f
Ivan Galban Smith <i.galban@lab.matcom.uh.cu>
2016-12-13 14:17:56 -0500

    implementing time_set() function

--- src/kernel/time.c
+++ src/kernel/time.c
@@ -9,7 +9,16 @@ void time_get(struct tm *t) {
 
 //Establece la fecha y la hora actuales.
 void time_set(struct tm *t) {
+	NMI_disable();
+	
+	set_RTC_register(REG_SECONDS, t->seconds);
+	set_RTC_register(REG_MINUTES, t->minutes);
+	set_RTC_register(REG_HOURS, t->hours);
+	set_RTC_register(REG_DAY, t->day);
+	set_RTC_register(REG_MONTH, t->month);
+	set_RTC_register(REG_YEAR, t->year);
 
+	NMI_enable();
 }
 
 
