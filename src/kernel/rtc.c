b8791962c5c7ab50abdd3dbf3d2acf7cb7a1f55a
Ivan Galban Smith <i.galban@lab.matcom.uh.cu>
2016-12-12 23:52:49 -0500

    fixing function rtc_get_*()
    is necesary disabled NMI before to read the data

--- src/kernel/rtc.c
+++ src/kernel/rtc.c
@@ -18,36 +18,42 @@ void rtc_init(){
 
 
 u8 rtc_get_seconds(){
+	NMI_disable();
 	outb(CMOS_ADDRESS, REG_SECONDS);
 	return inb(CMOS_DATA);
 }
 
 
 u8 rtc_get_minutes(){
+	NMI_disable();
 	outb(CMOS_ADDRESS, REG_MINUTES);
 	return inb(CMOS_DATA);
 }
 
 
 u8 rtc_get_hours(){
+	NMI_disable();
 	outb(CMOS_ADDRESS, REG_HOURS);
 	return inb(CMOS_DATA);
 }
 
 
 u8 rtc_get_day(){
+	NMI_disable();
 	outb(CMOS_ADDRESS, REG_DAY);
 	return inb(CMOS_DATA);
 }
 
 
 u8 rtc_get_month(){
+	NMI_disable();
 	outb(CMOS_ADDRESS, REG_MONTH);
 	return inb(CMOS_DATA);
 }
 
 
 u32 rtc_get_year(){
+	NMI_disable();
 	outd(CMOS_ADDRESS, REG_YEAR);
 	return ind(CMOS_DATA);
 }
