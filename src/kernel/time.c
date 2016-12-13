e71f632697f646410db5a86be14d01a01fb66492
Ivan Galban Smith <i.galban@lab.matcom.uh.cu>
2016-12-12 23:05:13 -0500

    implemented time_get() function

--- src/kernel/time.c
+++ src/kernel/time.c
@@ -1,14 +1,19 @@
 #include <time.h>
-
+#include <rtc.h>
 
 //Obtiene la fecha y la hora actuales.
-void time_get(struct tm *time) {
-
+void time_get(struct tm *t) {
+	t-> seconds = rtc_get_seconds();
+	t-> minutes = rtc_get_minutes();
+	t-> hours 	= rtc_get_hours();
+	t-> day 	= rtc_get_day();
+	t-> month 	= rtc_get_month();
+	t-> year 	= rtc_get_year();
 }
 
 
 //Establece la fecha y la hora actuales.
-void time_set(struct tm *time) {
+void time_set(struct tm *t) {
 
 }
 
